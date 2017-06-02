a26d406895bc7e2506871a0aaa573e871e3db5b6
imakine <imakine@ulb.ac.be>
2017-06-02 11:39:51 -0700

    Add files via upload

---
+++ Monte-carlo/myjob.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=test
+#
+# Account:
+#SBATCH --account=fc_neutronics
+#
+# Partition:
+#SBATCH --partition=savio
+#
+# QoS:
+#SBATCH --qos=nuclear_savio_normal
+#
+# Number of MPI tasks needed for use case (example):
+#SBATCH --ntasks=40
+#
+# Processors per task:
+#SBATCH --cpus-per-task=1
+#
+# Wall clock limit:
+#SBATCH --time=72:00:00
+#
+# SLURM Output File
+#SBATCH --output=slurm.out
+#
+# SLURM Error File
+#SBATCH --error=slurm.err
+#
+# Mail type:
+#SBATCH --mail-type=all
+#
+# Mail user:
+#SBATCH --mail-user=imakine@ulb.ac.be
+#
+## Command(s) to run (example):
+module load python
+module load numpy
+module load scipy
+python PonctualSP1.py
+        
